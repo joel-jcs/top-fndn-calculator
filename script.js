c64d66ee8d2c8095308070f36674b0e5ce10f4f3
joel-jcs <joeljcs@protonmail.com>
2024-09-04 14:49:02 -0400

    + added functionality for clear button (sets display to 0) + added number event listeners. gets added to display up to 14 digits

--- script.js
+++ script.js
@@ -27,9 +27,35 @@ const operate = (operator, operand1, operand2) => {
     }
 };
 
+const display = document.getElementById("display");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.getElementById("equals");
+const actionBtns = document.querySelectorAll(".action");
 
+// add numbers to display
+numberBtns.forEach(number => {
+    const MAX_DIGITS = 14;
+    number.addEventListener("click", () => {
+        if (display.textContent === "0") {
+            display.textContent = "";
+        }
+        if (display.textContent.length < MAX_DIGITS) {
+            display.textContent += number.value;
+        }
+    });
+});
 
-console.log(operate("+",1,4));
-console.log(operate("-",1,4));
-console.log(operate("*",1,4));
-console.log(operate("/",1,4));
+operatorBtns.forEach(operator => {
+    operator.addEventListener("click", () => {
+        
+    });
+});
+
+actionBtns.forEach(action => {
+    action.addEventListener("click", () => {
+        if (action.id === "clear") {
+            display.textContent = "0";
+        }
+    });
+});
